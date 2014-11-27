d20a98526f82e055b22692a92087228b695ce196
diwer <diwers@163.com>
2014-11-27 10:59:41 +0800

    小改动

--- demo1/script/controller.js
+++ demo1/script/controller.js
@@ -12,17 +12,31 @@ demo1.controller('HelloController', ['$scope', '$rootScope', function($scope, $r
         text: "hello world"
     };
     $rootScope.hello = "hi";
+    $scope.hello() {
+        alert("hi helloController");
+    }
+    $scope.roothello(sayhello) {
+        alert('rootscope say' + sayhello);
+    }
 }]);
 
 
 demo1.controller('fatherController', ['$scope', function($scope) {
 
     $scope.father = "father scope";
-
+    $scope.hello() {
+        alert("father");
+    }
 }]);
 demo1.controller('son1Controller', ['$scope', function($scope) {
     $scope.son = "i am son1 scope";
+    $scope.hello() {
+        alert("son1");
+    }
 }]);
 demo1.controller('son2Controller', ['$scope', function($scope) {
     $scope.son = "i am son2 scope";
+    $scope.hello() {
+        alert("son2");
+    }
 }]);
